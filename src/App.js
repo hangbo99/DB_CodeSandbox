5063534ff1edcefe5ddfe9875a909cb6f9d6d210
hangbo99 <102962030+hangbo99@users.noreply.github.com>
2022-05-21 00:37:49 +0900

    22.05.21 finish SongList

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
 import "./styles.css";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import MicIcon from "@mui/icons-material/Mic";
+import SongList from "./SongList";
+
+const Demo = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper
+}));
 
 const GundamHeader = () => {
   return <h3>건담</h3>;
@@ -8,11 +33,53 @@ const GundamTitle = () => {
   return <h2>건담 DB</h2>;
 };
 
+/*const getList = () => {
+  return (
+    <div>
+      <Grid item xs={12} md={6}>
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+          Avatar with text and icon
+        </Typography>
+        <Demo>
+          <List dense={dense}>
+            {generate(
+              <ListItem
+                secondaryAction={
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                }
+              >
+                <ListItemAvatar>
+                  <Avatar>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={secondary ? "Secondary text" : null}
+                />
+              </ListItem>
+            )}
+          </List>
+        </Demo>
+      </Grid>
+    </div>
+  );
+};*/
+
 export default function App() {
   return (
     <div className="App">
-      <GundamTitle />
-      <GundamHeader />
+      <AppBar position="relative">
+        <Toolbar>
+          <MicIcon sx={{ mr: 1 }} />
+          <Typography variant="h6" noWrap>
+            노래방 애창곡
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <SongList />
     </div>
   );
 }
---
+++ src/SongList.js
@@ -0,0 +1,98 @@
+import {
+   TextField,
+   Box,
+   Button,
+   List,
+   Paper,
+   ListItem,
+   ListItemText,
+   IconButton
+} from "@mui/material";
+
+import React, { useState } from "react";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import MusicVideo from "@mui/icons-material/MusicVideo";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const SongList = () => {
+   const [songName, setSongName] = useState("");
+   const [songs, setSongs] = useState(["씨발련"]);
+
+   const addSong = (e) => {
+      e.preventDefault();
+      console.log(songName);
+      setSongs([...songs, songName]);
+   };
+
+   //List에 하나의 아이템 반환 함수
+   const SongListItem = (props) => {
+      return (
+         <Paper>
+            <ListItem
+               Button
+               component="a"
+               href={`https://www.youtube.com/results?search_query=노래방+${props.title}`}
+               target="_blank"
+               sx={{ mb: 1, backgroundColor: "beige" }}
+               secondaryAction={
+                  <IconButton
+                     edge="end"
+                     onClick={(e) => {
+                        const deleted = [...songs];
+                        deleted.splice(props.idx, 1);
+                        setSongs(deleted);
+                        e.preventDefault();
+                     }}
+                  >
+                     <DeleteIcon />
+                  </IconButton>
+               }
+            >
+               <ListItemAvatar>
+                  <Avatar>
+                     <MusicVideo />
+                  </Avatar>
+               </ListItemAvatar>
+               <ListItemText
+                  primary={props.title}
+                  secondary={`https://www.youtube.com/results?search_query=노래방+${props.title}`}
+               />
+            </ListItem>
+         </Paper>
+      );
+   };
+
+   //Return SongList : Main 반환하는 느낌
+   return (
+      <>
+         <Box component="form" sx={{ mt: 3 }} onSubmit={addSong}>
+            <TextField
+               required
+               fullWidth
+               label="노래 제목"
+               name="SongInput"
+               id="SongInput"
+               value={songName}
+               onChange={(e) => setSongName(e.currentTarget.value)}
+            />
+            <Button
+               type="submit"
+               fullWidth
+               variant="contained"
+               sx={{ mt: 1, mb: 3 }}
+            >
+               노래 추가
+            </Button>
+         </Box>
+
+         <List>
+            {songs.map((song, index) => (
+               <SongListItem key={index} idx={index} title={song} />
+            ))}
+         </List>
+      </>
+   );
+};
+
+export default SongList;
